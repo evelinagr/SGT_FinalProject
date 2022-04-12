1b7b21a0d344f54ee3f71250fa0e007fd5078b63
Sanita-KR <98774045+Sanita-KR@users.noreply.github.com>
2022-04-12 23:04:54 +0300

    Work in progress..

--- app.js
+++ app.js
@@ -1,88 +1,37 @@
-// Grab the card data
-let otherArray = []
-let stuff = document.querySelectorAll(".price")
-stuff.forEach((element, index) => {
-     let theObject = {}
-     let name = element.parentElement.firstElementChild.innerHTML
-     let image =element.parentNode.previousElementSibling.getAttribute("src")
-     let price = element.firstElementChild.innerHTML
-
-     theObject['productName'] = name;
-     theObject['price'] = price;
-     theObject['imageSrc'] = image;
-/// Add the click function to the add to cart button 
-     element.nextElementSibling.addEventListener("click", (e) => {
-          let objectId = e.target.getAttribute("data-id")
-          if(otherArray.indexOf(theObject) == -1){
-               otherArray.push(theObject) ;
-                addItem(theObject, objectId)
-          } 
-          event.preventDefault();
-     })
-     
-})
-// This adds the item to the table
-let addItem = (theObject, objectId) => {
-     theObject['id'] = objectId;
-     let tableRow = document.createElement("tr");
-     let tableData = document.createElement("td");
-     tableRow.innerHTML = 
-     `
-     <td><img src=${theObject.imageSrc} width="100%"/></td>
-     <td>${theObject.productName}</td>
-     <td>${theObject.price}</td>
-     <td>
-        <a href="#" class="remove" title="Remove from cart" data-id="${theObject.id}">X</a>
-    </td>
-     `
-     document.querySelectorAll("table#cart-content tbody")[0].appendChild(tableRow)
-     /// And activated the delete item button
-     buttonActivator()
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+function ready () {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    console.log(removeCartItemButtons)
+    for (var i = 0; i < removeCartItemButtons.length; i++){
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
 }
 
-let buttonActivator = () => {
-     let removeButtons =  document.querySelectorAll(".remove")
+function removeCartItem(event) {
+            var buttonClicked = event.target
+            buttonClicked.parentElement.parentElement.remove()
+            updateCartTotal()
 
-removeButtons.forEach(el => {
-     el.addEventListener("click", (e)=>{
-          let row = e.target.parentNode.parentNode;
-          document.querySelectorAll("table#cart-content tbody")[0].removeChild(row)
-          event.preventDefault();
-     })
-})
-
-}
-/// For clearing the entire cart
-document.querySelector("a#clear-cart").addEventListener("click", ()=>{
-     let rows = document.querySelectorAll("table#cart-content tbody")[0].childNodes
-     while(rows)
-          document.querySelectorAll("table#cart-content tbody")[0].removeChild(rows[0])  
-})
-
-
-
-
-////mouseover and mouseout
-
-function setNewImage()
-{
-document.getElementById("hero").src = "images/Lovehateswitch2.png";
-}
-
-function setOldImage(){
-document.getElementById("hero").src = "images/lovehateswitch.jpg";
 }
 
 
-//flip
-
-// const product = document.getElementById("slides-box")
-
-// product.addEventListener("click",flipProduct);
-
-// function flipProduct() {
-//      product.classList.toggle("flipProduct");
-// }
-
-
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('€', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+       }
+       document.getElementsByClassName('cart-total-price')[0].innerText = '€' + total
+}
 
