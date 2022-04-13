3900b903bfe68221cbf755dc89447315458524d4
ievamoon <98773992+ievamoon@users.noreply.github.com>
2022-04-13 21:23:15 +0300

    updated blog

--- script_blog.js
+++ script_blog.js
@@ -1,7 +1,11 @@
-$(function() {
-    $('#readmore').click(function() {
-        $('#hello').animate({ height: '100%' });
-        $(this).hide();
-        return false;
+$('#readmore').click(function() {
+    $('#hello').animate({ height: '100%' });
+    $(this).hide();
+    $('#readless').show(function() {
+        $('#readless').click(function() {
+            $('#hello').animate({ height: '100px' });
+            $(this).hide();
+            $('#readmore').show();
+        });
     });
 });
