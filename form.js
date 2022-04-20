ef0ed48925fff59b241cd16c710aef741c237834
37129 <evelinagrebneva@inbox.lv>
2022-04-20 13:32:24 +0300

    changes

---
+++ form.js
@@ -0,0 +1,11 @@
+function submitData(){ 
+    var name =$('#name').val();
+    var email =$('#email').val();
+
+    jsonObject={
+        "name":"",
+        "email":"",
+    }
+    jsonObject.id=name;
+    jsonObject.name=name;
+}
