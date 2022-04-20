2e636c503929e5297eb5078fa4299259f0a83b09
37129 <evelinagrebneva@inbox.lv>
2022-04-20 16:53:36 +0300

    rotation

--- cards.js
+++ cards.js
@@ -1,8 +1,20 @@
-const card = document.querySelector('.card-box');
+const cardBoxes = document.querySelectorAll('.card-box');
 
-card.addEventListener('mousemove', rotate)
+for(let i=0; i <cardBoxes.length; i++){
+    const cardBox = cardBoxes[i];
+    cardBox.addEventListener('mousemove', rotate)
+    cardBox.addEventListener('mouseout', stopRotate)
+}
 
 
 function rotate(event) {
-    console.log('cli');
+    const card = this.querySelector('.card');
+    const halfHeight = card.offsetHeight / 2;
+
+    card.style.transform = 'rotateX(' + - (event.offsetY - halfHeight) / 10 +'deg) rotateY(' +(event.offsetX - halfHeight) / 5 + 'deg)'
+}
+
+function stopRotate(event){
+    const card = this.querySelector('.card');
+    card.style.transform = 'rotateX(0)';
 }
